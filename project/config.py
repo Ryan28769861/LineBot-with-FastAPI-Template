2924cd3d15858a57e4fde1f7d4890a3f4b445bf0
Ryan28769861 <126656595+Ryan28769861@users.noreply.github.com>
2024-08-01 11:48:41 +0800

    config.py

--- project/config.py
+++ project/config.py
@@ -1,10 +1,9 @@
 import os
-
 from dotenv import load_dotenv
-
 # Load environment variables
 load_dotenv()
-
 # LINE Bot 設定
-LINE_CHANNEL_SECRET = os.environ.get("LINE_CHANNEL_SECRET")
-LINE_CHANNEL_ACCESS_TOKEN = os.environ.get("LINE_CHANNEL_ACCESS_TOKEN")
+LINE_CHANNEL_SECRET = os.environ.get("932a1e392cfa87077343f2d0cb5b59c9")
+LINE_CHANNEL_ACCESS_TOKEN = os.environ.get("phKWDwx8E0dhzEL1NVYFdatZfhOsxsiPnG3EYLtEtMiTImU4NzbSCXLyiBSxkKrkygEnpKAsVAcBrS1cNxbOV2QrNL+JyNEbU+qRj9/7BudfaNz1EEoWF0BZQAk5QNE9eDhKSndPU3MoyI/TTSS7zgdB04t89/1O/w1cDnyilFU=")
+GOOGLE_API_KEY = os.environ.get('YAIzaSyA_3frvb0mASVcrvmB-ygBv_ldGg5dved0')
+FIREBASE_URL = os.environ.get('https://gemini-99f7d-default-rtdb.firebaseio.com/')
